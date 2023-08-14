c28335b292a37290a2f2c7e14d66182e31472e42
Devskiller <owl@devskiller.com>
2023-08-14 14:16:03 +0000

    File modification

--- terraform/code.tf
+++ terraform/code.tf
@@ -1,8 +1,28 @@
-provider "aws" {
-    region = "ami-0be656e75e69af1a9"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "0.0.0.0/0"
+
+  tags = {
+    Name = "tf-example"
+  }
 }
 
-resource "aws_instance" "Demo_EC2" {
-    ami = "ami-0be656e75e69af1a9"
-    instance_type = "t2.micro"
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.my_vpc.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.my_vpc.ipv6_cidr_block]
+  }
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0be656e75e69af1a9" 
+  instance_type = "t2.micro"
+  owner = ["masters-of-destruction"]
+
 }
